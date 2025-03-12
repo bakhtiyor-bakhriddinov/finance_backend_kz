7cdfb6c75a547bd09b232d9a597fe5743401cc1c
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-03-12 11:49:57 +0500

    Added new features

--- models/departments.py
+++ models/departments.py
@@ -15,7 +15,7 @@ class Departments(Base):
     is_active = Column(Boolean, default=True)
     user_id = Column(UUID, ForeignKey("users.id", ondelete="SET NULL"), unique=True)
     head = relationship('Users', back_populates='department', lazy="joined")
-    requests = relationship('Requests', back_populates='department', lazy="joined", passive_deletes=True)
+    requests = relationship('Requests', back_populates='department', passive_deletes=True)
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
